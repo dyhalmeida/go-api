d860b2e1ab6da015e10b24e9b74f6d540d3d58b4
dyhalmeida <dyhalmeida@gmail.com>
2025-02-08 10:29:09 -0300

    feat: set up HTTP server with chi router

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,35 @@
 package main
 
-import "github.com/dyhalmeida/go-apis/configs"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/dyhalmeida/go-apis/configs"
+	"github.com/dyhalmeida/go-apis/internal/entity"
+	"github.com/dyhalmeida/go-apis/internal/infra/database"
+	"github.com/dyhalmeida/go-apis/internal/infra/websever/handlers"
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/chi/v5"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
 
 func main() {
 	config := configs.NewConfig()
-	println(config.GetDatabaseDriver())
+
+	db, err := gorm.Open(sqlite.Open(("test.db")), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	db.AutoMigrate(&entity.User{}, &entity.Product{})
+
+	productDB := database.NewProductDb(db)
+	productHandler := handlers.NewProductHandler(productDB)
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Post("/products", productHandler.CreateProduct)
+
+	http.ListenAndServe(fmt.Sprintf(":%s", config.GetServerPort()), r)
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/dyhalmeida/go-apis
 go 1.22.2
 
 require (
+	github.com/go-chi/chi v1.5.1
+	github.com/go-chi/chi/v5 v5.2.0
 	github.com/go-chi/jwtauth v1.2.0
 	github.com/google/uuid v1.4.0
 	github.com/spf13/viper v1.19.0
